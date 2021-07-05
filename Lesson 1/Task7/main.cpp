475b13c7a48126aaba0bf98cb6c1f59e824c848f
Danil <danilkovalev1306@gmail.com>
2021-07-06 00:54:55 +0300

    add Task7

---
+++ Lesson 1/Task7/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int temperature = 0, mode = 0;
+    cout << "Select mode: ";
+    cin >> mode;
+    switch(mode) {
+    case 1:
+        cout << "Enter temperature: ";
+        cin >> temperature;
+        if (temperature >= 500 && temperature < 600) {
+            cout << "[CORE]: Overheat mode";
+        }
+        if (temperature >= 600 && temperature < 700) {
+            cout << "[CORE]: Cooldown required";
+        }
+        if (temperature >= 700) {
+            cout << "[CORE]: Cooldowm started!";
+        }
+        break;
+    case 2:
+        cout << "Enter temperature: ";
+        cin >> temperature;
+        cout <<  "[CORE]: Using default configuration" << endl;
+        if (temperature == 300) {
+            cout << "[CORE]: Bad configuration detected. Stopping down core";
+        }
+        if (temperature >= 500 && temperature < 600) {
+            cout << "[CORE]: Overheat mode";
+        }
+        if (temperature >= 600 && temperature < 700) {
+            cout << "[CORE]: Cooldown required";
+        }
+        if (temperature >= 700) {
+            cout << "[CORE]: Cooldowm started!";
+        }
+        break;
+    }
+}
