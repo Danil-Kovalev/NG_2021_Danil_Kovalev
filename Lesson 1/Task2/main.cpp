5649574f9cf7c22e36082c952818bb556edd8c32
Danil <danilkovalev1306@gmail.com>
2021-07-05 14:26:08 +0300

    add Task2

---
+++ Lesson 1/Task2/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int first_number = 0, second_number = 0;
+    cout << "Enter first number: ";
+    cin >> first_number;
+    cout << "Enter second number: ";
+    cin >> second_number;
+    cout << "Result: " << first_number + second_number;
+}
