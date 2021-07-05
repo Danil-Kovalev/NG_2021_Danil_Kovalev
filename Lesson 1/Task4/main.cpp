9d96b1913c649367541519e28a2a12d7914046d8
Danil <danilkovalev1306@gmail.com>
2021-07-05 22:43:34 +0300

    fixed code style

--- Lesson 1/Task4/main.cpp
+++ Lesson 1/Task4/main.cpp
@@ -11,10 +11,12 @@ int main()
         cout << " Rabotai bolshe ";
     }
     if ( salary > 999 ) {
-        if ( salary > 999999 )
+        if ( salary > 999999 ) {
             cout << " Ti millioner! ";
-        if ( salary < 1000000 )
+        }
+        if ( salary < 1000000 ) {
             cout << " Kruto! ";
+        }
     }
     cout << ", no ti molodec! ";
 }
