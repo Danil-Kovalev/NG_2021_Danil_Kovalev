6172b079852ad9ef98a469ab999e6e9d13cc5c03
Danil <danilkovalev1306@gmail.com>
2021-07-07 02:08:30 +0300

    update Task5

--- Lesson 1/Task5/main.cpp
+++ Lesson 1/Task5/main.cpp
@@ -4,31 +4,28 @@ using namespace std;
 
 int main()
 {
-    int first_number = 0, second_number = 0, result = 0;
+    int first_number = 0, second_number = 0, act = 0;
     cout << "Vvedite pervoe chislo: ";
     cin >> first_number;
     cout << "Vvedite vtoroe chislo: ";
     cin >> second_number;
     cout << "Vvedite deistvie: ";
-    cin >> result;
-    if (result > 0 && result < 5) {
-        switch(result) {
-        case 1:
-            result = first_number + second_number;
-            break;
-        case 2:
-            result = first_number - second_number;
-            break;
-        case 3:
-            result = first_number * second_number;
-            break;
-        case 4:
-            result = first_number / second_number;
-            break;
-        }
-        cout << "Resultat: " << result;
-    }
-    else {
+    cin >> act;
+    cout << "Resultat: ";
+    switch(act) {
+    case 1:
+        cout << first_number + second_number;
+        break;
+    case 2:
+        cout << first_number - second_number;
+        break;
+    case 3:
+        cout << first_number * second_number;
+        break;
+    case 4:
+        cout << first_number / second_number;
+        break;
+    default:
         cout << "Error...";
     }
 }
