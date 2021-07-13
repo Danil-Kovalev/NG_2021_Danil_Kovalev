9d5a100169a7b26cae20b69b933f0af5687b1805
Danil <danilkovalev1306@gmail.com>
2021-07-13 12:36:27 +0300

    rename cycles

--- Lesson 2/Task3/main.cpp
+++ Lesson 2/Task3/main.cpp
@@ -7,8 +7,8 @@ int main()
     int inputUser = 0;
     cout << "Enter number: ";
     cin >> inputUser;
-    for (int i = 0; i < inputUser; i++) {
-        for (int i = 0; i < inputUser; i++) {
+    for (int transition = 0; transition < inputUser; transition++) {
+        for (int star = 0; star < inputUser; star++) {
             cout << "*";
         }
         cout << endl;
