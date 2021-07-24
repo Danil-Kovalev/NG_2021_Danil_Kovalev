a439922b7b2e71678812255a8323e473261a1242
Danil <danilkovalev1306@gmail.com>
2021-07-24 14:20:11 +0300

    added Task4

---
+++ Lesson 3/Task4/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int inputUser [20], maxNumber = 0;
+    for (int i = 0; i < 20; i++) {
+        cout << "Enter " << i + 1 << " number: ";
+        cin >> inputUser[i];
+        if (maxNumber < inputUser[i])
+            maxNumber = inputUser[i];
+        if (inputUser[i] == 0)
+            break;
+    }
+    for (int i = 0; i < maxNumber; i++) {
+        for (int j = 0; j < maxNumber - (inputUser[i] + j); j++) {
+            cout << " ";
+        }
+        for (int j = 0; j != inputUser[i]; j++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
