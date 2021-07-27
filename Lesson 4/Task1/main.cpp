5a94229a60d51bef45c196c2479330c647363464
Danil <danilkovalev1306@gmail.com>
2021-07-27 15:44:28 +0300

    added Task1

---
+++ Lesson 4/Task1/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char inputUser[100];
+    int words = 0, count = 0, i = 0;
+    cin.getline(inputUser, 100);
+    while (inputUser[i] != 0) {
+        if(count == 0) {
+            if ((inputUser[i] >= 'a' && inputUser[i] <= 'z') || (inputUser[i] >= 'A' && inputUser[i] <= 'Z')) {
+                count = 1;
+                words++;
+            }
+        }
+        else if ((inputUser[i] >= 0 && inputUser[i] <= 64) || (inputUser[i] >= 91 && inputUser[i] <= 96) || (inputUser[i] >= 123 && inputUser[i] <= 126) || inputUser[i] == ' ') {
+            count = 0;
+        }
+        i++;
+    }
+    cout << "Number words: " << words;
+}
