4a90a0ddb1e0c88b30fea77d1691f4f3d08cee90
Danil <danilkovalev1306@gmail.com>
2021-07-30 14:21:06 +0300

    added stringInput for inputUser

--- Lesson 4/Task3/main.cpp
+++ Lesson 4/Task3/main.cpp
@@ -4,15 +4,17 @@ using namespace std;
 
 int main()
 {
-    char inputUser[100];
+    char inputUser[100], stringInput;
     int i = 0;
     cout << "Enter the string: ";
     cin.getline(inputUser, 100);
+    cout << "Uppercase string: ";
     while (inputUser[i] != 0) {
         if (inputUser[i] >= 'a' && inputUser[i] <= 'z') {
-            inputUser[i] -= 32;
+            stringInput = inputUser[i];
+            stringInput -= 32;
+            cout << stringInput;
         }
         i++;
     }
-    cout << "Uppercase string: " << inputUser;
 }
