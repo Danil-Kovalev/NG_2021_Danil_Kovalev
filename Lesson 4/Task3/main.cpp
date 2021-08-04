30cce6683d4c274bb72642087b165507da601693
Danil <danilkovalev1306@gmail.com>
2021-08-04 11:09:48 +0300

    adding else and fixed if

--- Lesson 4/Task3/main.cpp
+++ Lesson 4/Task3/main.cpp
@@ -4,16 +4,17 @@ using namespace std;
 
 int main()
 {
-    char inputUser[100], stringInput;
+    char inputUser[100];
     int i = 0;
     cout << "Enter the string: ";
     cin.getline(inputUser, 100);
     cout << "Uppercase string: ";
     while (inputUser[i] != 0) {
         if (inputUser[i] >= 'a' && inputUser[i] <= 'z') {
-            stringInput = inputUser[i];
-            stringInput -= 32;
-            cout << stringInput;
+            cout << char(inputUser[i] - 32);
+        }
+        else {
+            cout << inputUser[i];
         }
         i++;
     }
