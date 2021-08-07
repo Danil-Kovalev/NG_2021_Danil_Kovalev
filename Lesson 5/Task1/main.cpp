de9d383bb160e4352186b499f6b4e80e46117429
Danil <danilkovalev1306@gmail.com>
2021-08-07 19:30:39 +0300

    simplification of the task

--- Lesson 5/Task1/main.cpp
+++ Lesson 5/Task1/main.cpp
@@ -8,39 +8,32 @@ int getInputUser(int inputUser = 0) {
     return inputUser;
 }
 
-void drawSpace (int inputUser = 0) {
-    int i = 0;
-    for (int space = inputUser - 1; space > i; space--) {
-        cout << " ";
-    }
-}
-
-void drawStar (int i = 0) {
-    for (int star = 0; star <= i * 2; star++) {
-        cout << "*";
+void drawSymbol (int size, char symbol) {
+    for (int star = 0; star < size; star++) {
+        cout << symbol;
     }
 }
 
 void drawBranches (int inputUser = 0) {
-    for (int i = 0; i < inputUser; i++) {
-        drawSpace(inputUser);
-        drawStar(i);
+    for (int i = 1; i <= inputUser; i++) {
+        drawSymbol(inputUser - i, ' ');
+        drawSymbol(i * 2 - 1, '*');
         cout << endl;
     }
 }
 
-void drawTreePillar (int inputUser = 0) {
-    int space = 0;
-    for (space = inputUser; space > 1; space--) {
-        cout << " ";
-    }
+void drawTreePillar (int size) {
+    drawSymbol(size - 1, ' ');
     cout << "*";
+}
+
+void drawTree (int size) {
+    drawBranches(size);
+    drawTreePillar(size);
 }
 
 int main()
 {
-    int inputUser = getInputUser();
-    drawBranches(inputUser);
-    drawTreePillar(inputUser);
+    drawTree(getInputUser());
 
 }
